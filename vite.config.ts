df79e58ed890551f366245b3e0ab4b23a05669b2
Shrey Tailor <shreym.tailor@gmail.com>
2024-02-17 21:33:57 +1100

    Changing the base again (#11)

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://shreytailor.github.io/personal-website',
+  base: '/personal-website/',
 });
