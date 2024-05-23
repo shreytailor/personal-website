a26d7c5e731e4a54e0aadb12f23261f80c6c1de1
Shrey Tailor <shreytailor@canva.com>
2024-05-24 00:02:13 +1000

    Removing build errors

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,31 +1,11 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Navigation.module.css';
 
 export default function Navigation() {
-  const { pathname } = useLocation();
-
   const Name = () => (
     <h1>
       <Link to="/">Shrey Tailor</Link>
     </h1>
-  );
-
-  const Links = () => (
-    <ul>
-      <li>
-        <Link to="/" className={pathname === '/' ? styles.underline : ''}>
-          Work
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/contact"
-          className={pathname === '/contact' ? styles.underline : ''}
-        >
-          Contact
-        </Link>
-      </li>
-    </ul>
   );
 
   const Navigation = (
