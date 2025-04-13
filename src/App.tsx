a0cf7eadf9d70d6a847abf5b623a34db21a926c2
Shrey Tailor <shreym.tailor@gmail.com>
2025-04-13 14:11:56 +1000

    Param based navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import Navigation from './components/Navigation';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styles from './App.module.css';
+import { useEffect } from 'react';
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const page = params.get('page');
+
+    if (page) {
+      navigate(`/${page}`, { replace: true });
+    }
+  }, [location.search, navigate]);
+
   return (
     <div className={styles.container}>
       <Navigation />
