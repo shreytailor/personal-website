3905e3481f5b332526898cd8191f5ae40c2d2b02
Shrey Tailor <shreym.tailor@gmail.com>
2025-07-28 23:42:02 +1000

    First visit confetti

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Navigation from './components/Navigation';
 import { Outlet } from 'react-router-dom';
 import styles from './App.module.css';
+import Confetti from 'react-confetti';
 
 function App() {
+  const hasVisited = localStorage.getItem('hasVisited');
+  if (!hasVisited) {
+    localStorage.setItem('hasVisited', 'true');
+  }
+
   return (
     <div className={styles.container}>
+      {!hasVisited && <Confetti recycle={false} numberOfPieces={125} />}
       <Navigation />
       <Outlet />
     </div>
